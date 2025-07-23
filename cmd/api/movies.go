c17c8407b1f07b56fcd0810a8a46f72a3315ae76
dbrsnko <contactdenysborysenko@gmail.com>
2025-07-23 14:51:05 +0300

    JSON responses for movies and health

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
-	"github.com/go-chi/chi"
+	"greenlight/internal/data"
 	"net/http"
-	"strconv"
+	"time"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -12,11 +12,22 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 }
 
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
-	params := chi.URLParam(r, "id")
-	id, err := strconv.ParseInt(params, 10, 64)
-	if err != nil || id < 1 {
+	id, err := app.readIDParam(r)
+	if err != nil {
 		http.NotFound(w, r)
 		return
 	}
-	fmt.Fprintf(w, "show the details of movie %d\n", id)
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"drama", "romance", "war"},
+		Version:   1,
+	}
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
 }
